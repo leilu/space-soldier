b431ad8d5463e6c017a494d6b9e59ac2e8945dbd
Andrew W Hung <druphoria@utexas.edu>
2016-06-25 11:00:20 -0700

    collision and dashing fixes

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,92 +2,74 @@
 using SpriteTile;
 
 public class PlayerMovement : MonoBehaviour {
-    private Rigidbody2D rb;
-    private BoxCollider2D boxCollider;
-    private Animator animator;
-    private Dash dash;
-
     public float speed;
     public float collisionDistance;
     public int wallSpriteTileLayer;
+    public float colliderMovementMultiplier;
 
-	void Start () {
-        rb = GetComponent<Rigidbody2D>();
+
+    private Rigidbody2D rb2d;
+    private BoxCollider2D boxCollider;
+    private Animator animator;
+    private Dash dash;
+    private float inputX, inputY;
+
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
         dash = GameObject.Find("Dash").GetComponent<Dash>();
 	}
 
-    void Update()
+    void FixedUpdate()
     {
         if (GameState.Paused || GameState.InputLocked || dash.IsDashing())
         {
             return;
         }
 
-        float inputX = Input.GetAxisRaw("Horizontal");
-        float inputY = Input.GetAxisRaw("Vertical");
-        float halfCollisionDistance = collisionDistance / 2;
- 
-        if(GameState.TutorialMode && (inputX != 0 || inputY != 0))
+        inputX = Input.GetAxisRaw("Horizontal");
+        inputY = Input.GetAxisRaw("Vertical");
+
+        if (GameState.TutorialMode && (inputX != 0 || inputY != 0))
         {
             TutorialEngine.Instance.Trigger(TutorialTrigger.Walk);
         }
 
-        Vector2 newVelocity = new Vector2(inputX, inputY).normalized * speed;
+        Vector2 newVelocity = dash.IsDashing() ? rb2d.velocity : new Vector2(inputX, inputY).normalized * speed;
 
-        if ((CollisionCheck(rb.position.x, rb.position.y + collisionDistance)
-            || CollisionCheck(rb.position.x - halfCollisionDistance, rb.position.y + collisionDistance)
-            || CollisionCheck(rb.position.x + halfCollisionDistance, rb.position.y + collisionDistance)
-            || Physics2D.BoxCast(rb.position, boxCollider.size, 0f, Vector2.up, halfCollisionDistance,
-                LayerMasks.ObstacleLayerMask).transform != null)
-            && inputY > 0)
+        Vector2 colliderMin = new Vector2(boxCollider.bounds.min.x + colliderMovementMultiplier * boxCollider.bounds.extents.x,
+            boxCollider.bounds.min.y + colliderMovementMultiplier * boxCollider.bounds.extents.y);
+        Vector2 colliderMax = new Vector2(boxCollider.bounds.max.x - colliderMovementMultiplier * boxCollider.bounds.extents.x,
+            boxCollider.bounds.max.y - colliderMovementMultiplier * boxCollider.bounds.extents.y);
+
+        if (Physics2D.OverlapArea(new Vector2(colliderMin.x + collisionDistance, colliderMin.y), 
+            new Vector2(colliderMax.x + collisionDistance, colliderMax.y), LayerMasks.MovementObstructedLayerMask) != null)
         {
-            newVelocity.y = 0;
-        }
-        if ((CollisionCheck(rb.position.x, rb.position.y - collisionDistance)
-            || CollisionCheck(rb.position.x - halfCollisionDistance, rb.position.y - collisionDistance)
-            || CollisionCheck(rb.position.x + halfCollisionDistance, rb.position.y - collisionDistance)
-            || Physics2D.BoxCast(rb.position, boxCollider.size, 0f, Vector2.down, halfCollisionDistance,
-                LayerMasks.ObstacleLayerMask).transform != null)
-            && inputY < 0)
-        {
-            newVelocity.y = 0;
-        }
-        if ((CollisionCheck(rb.position.x + collisionDistance, rb.position.y)
-            || CollisionCheck(rb.position.x + collisionDistance, rb.position.y - halfCollisionDistance)
-            || CollisionCheck(rb.position.x + collisionDistance, rb.position.y + halfCollisionDistance)
-            || Physics2D.BoxCast(rb.position, boxCollider.size, 0f, Vector2.right, halfCollisionDistance,
-                LayerMasks.ObstacleLayerMask).transform != null)
-            && inputX > 0)
-        {
-            newVelocity.x = 0;
-        }
-        if ((CollisionCheck(rb.position.x - collisionDistance, rb.position.y)
-            || CollisionCheck(rb.position.x - collisionDistance, rb.position.y - halfCollisionDistance)
-            || CollisionCheck(rb.position.x - collisionDistance, rb.position.y + halfCollisionDistance)
-            || Physics2D.BoxCast(rb.position, boxCollider.size, 0f, Vector2.left, halfCollisionDistance,
-                LayerMasks.ObstacleLayerMask).transform != null)
-            && inputX < 0)
-        {
-            newVelocity.x = 0;
+            newVelocity.x = Mathf.Min(0, newVelocity.x);
         }
 
-        rb.velocity = newVelocity;
+        if (Physics2D.OverlapArea(new Vector2(colliderMin.x - collisionDistance, colliderMin.y),
+            new Vector2(colliderMax.x - collisionDistance, colliderMax.y), LayerMasks.MovementObstructedLayerMask) != null)
+        {
+            newVelocity.x = Mathf.Max(0, newVelocity.x);
+        }
+
+        if (Physics2D.OverlapArea(new Vector2(colliderMin.x, colliderMin.y + collisionDistance),
+            new Vector2(colliderMax.x, colliderMax.y + collisionDistance), LayerMasks.MovementObstructedLayerMask) != null)
+        {
+            newVelocity.y = Mathf.Min(0, newVelocity.y);
+        }
+
+        if (Physics2D.OverlapArea(new Vector2(colliderMin.x, colliderMin.y - collisionDistance),
+            new Vector2(colliderMax.x, colliderMax.y - collisionDistance), LayerMasks.MovementObstructedLayerMask) != null)
+        {
+            newVelocity.y = Mathf.Max(0, newVelocity.y);
+        }
+
+        rb2d.velocity = newVelocity;
         animator.SetInteger("HorizontalAxis", (int)newVelocity.x);
         animator.SetInteger("VerticalAxis", (int)newVelocity.y);
     }
 
-    bool CollisionCheck(float x, float y)
-    {
-        Vector2 pos = new Vector2(x, y);
-
-        if (LoadLevel.TestingCityLevel)
-        {
-            return false;
-        }
-        return Tile.GetCollider(pos, BasicLevelDecorator.CliffTileLayer)
-            || Tile.GetCollider(pos, BasicLevelDecorator.WaterTileLayer)
-            || Tile.GetCollider(pos, BasicLevelDecorator.TreeTileLayer);
-    }
 }
--- Assets/Scripts/Weapons/Abilities/Dash.cs
+++ Assets/Scripts/Weapons/Abilities/Dash.cs
@@ -7,25 +7,35 @@ public class Dash : Weapon {
     [SerializeField]
     private float energyRequirement;
     [SerializeField]
-    private float dashDistance;
+    private float maxDashDistance;
     [SerializeField]
     private float dashSpeed;
     [SerializeField]
     private float targetDistanceStopThreshold;
 
-    private bool dashing = false;
+    public bool dashing = false;
     private Vector2 target;
     private Rigidbody2D rb2d;
+    private float dashStartTime = 0;
+    private float calculatedDashDistance;
+    private BoxCollider2D boxCollider2D;
 
     public override float Click ()
     {
-        if (!dashing)
+        if (!dashing && CanFire())
         {
+            nextFiringTime = Time.time + FiringDelay;
             dashing = true;
+            dashStartTime = Time.time;
             Vector3 dashDir = VectorUtil.DirectionToMousePointer(Player.PlayerTransform);
-            target = Player.PlayerTransform.position + dashDir * dashDistance;
+            RaycastHit2D obstacle = Physics2D.BoxCast((Vector2)Player.PlayerTransform.position + boxCollider2D.offset, boxCollider2D.size, 0, dashDir,
+                maxDashDistance, LayerMasks.MovementObstructedLayerMask);
+
+            calculatedDashDistance = obstacle.transform == null ? maxDashDistance : obstacle.distance;
+
+            target = Player.PlayerTransform.position + dashDir * calculatedDashDistance;
             rb2d.velocity = dashDir * dashSpeed;
-            return energyCost;
+            return energyRequirement;
         }
 
         return 0;
@@ -33,10 +43,14 @@ public class Dash : Weapon {
 
     void FixedUpdate ()
     {
-        if ((rb2d.position - target).magnitude < targetDistanceStopThreshold)
+        if (dashing)
         {
-            dashing = false;
-            rb2d.velocity = Vector2.zero;
+            float distanceTraveled = Mathf.Abs((Time.time - dashStartTime) * rb2d.velocity.magnitude);
+            if (distanceTraveled >= calculatedDashDistance || (rb2d.position - target).magnitude < targetDistanceStopThreshold)
+            {
+                dashing = false;
+                rb2d.velocity = Vector2.zero;
+            }
         }
     }
 
@@ -65,10 +79,16 @@ public class Dash : Weapon {
     void Start()
     {
         rb2d = Player.PlayerTransform.GetComponent<Rigidbody2D>();
+        boxCollider2D = Player.PlayerTransform.GetComponent<BoxCollider2D>();
     }
 
     public bool IsDashing()
     {
         return dashing;
+    }
+
+    public void StopDash()
+    {
+        dashing = false;
     }
 }
