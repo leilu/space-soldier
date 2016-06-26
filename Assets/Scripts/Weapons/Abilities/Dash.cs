9a00749f862a85faf453a1b41eaf823daf4ed2fa
Andrew W Hung <druphoria@utexas.edu>
2016-06-26 12:35:05 -0700

    dash waypoints

--- Assets/Scripts/Weapons/Abilities/Dash.cs
+++ Assets/Scripts/Weapons/Abilities/Dash.cs
@@ -1,6 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System;
 using System.Collections.Generic;
 
 public class Dash : Weapon {
@@ -16,48 +14,88 @@ public class Dash : Weapon {
     private float boxCastOffset;
 
     public bool dashing = false;
-    private Vector2 target;
+    private int waypointIndex;
     private Rigidbody2D rb2d;
     private float dashStartTime = 0;
     private BoxCollider2D boxCollider2D;
+    private List<Vector2> waypoints;
+    private RaycastHit2D obstacle;
 
     public override float Click ()
     {
         if (!dashing && CanFire())
         {
+            waypoints.Clear();
+            waypointIndex = 0;
+
             Vector3 dashDir = VectorUtil.DirectionToMousePointer(Player.PlayerTransform);
             Vector2 boxCastOffsetVector = boxCastOffset * dashDir;
-            RaycastHit2D obstacle = Physics2D.BoxCast((Vector2)Player.PlayerTransform.position + boxCollider2D.offset + boxCastOffsetVector,
+            obstacle = Physics2D.BoxCast((Vector2)Player.PlayerTransform.position + boxCollider2D.offset + boxCastOffsetVector,
                 boxCollider2D.size, 0, dashDir, maxDashDistance, LayerMasks.MovementObstructedLayerMask);
 
-            rb2d.velocity = Vector2.zero;
-            float calculatedDashDistance = obstacle.transform == null ? maxDashDistance : obstacle.distance + boxCastOffset;
-
-            if (calculatedDashDistance <= boxCastOffset)
+            if (obstacle.transform != null)
             {
-                return 0;
+                float originalDistance = obstacle.distance + boxCastOffset;
+
+                Vector2 currPos = Player.PlayerTransform.position + dashDir * originalDistance;
+
+                if (originalDistance > boxCastOffset)
+                {
+                    waypoints.Add(currPos);
+                }
+
+                Vector2 newDir = CalculateNewDirection(dashDir, obstacle.normal);
+                if (newDir != Vector2.zero)
+                {
+                    obstacle = Physics2D.BoxCast(currPos + .05f * obstacle.normal, boxCollider2D.size, 0, newDir, maxDashDistance,
+                        LayerMasks.MovementObstructedLayerMask);
+                    currPos += newDir * (obstacle.transform == null ? maxDashDistance - originalDistance :
+                        Mathf.Min(obstacle.distance, maxDashDistance - originalDistance));
+                    waypoints.Add(currPos);
+                } else if (originalDistance <= boxCastOffset)
+                {
+                    return 0;
+                }
+            } else
+            {
+                waypoints.Add(Player.PlayerTransform.position + dashDir * maxDashDistance);
             }
 
+            rb2d.velocity = Vector2.zero;
             nextFiringTime = Time.time + FiringDelay;
             dashing = true;
             dashStartTime = Time.time;
 
-            target = Player.PlayerTransform.position + dashDir * calculatedDashDistance;
             return energyRequirement;
         }
 
         return 0;
     }
 
+    Vector2 CalculateNewDirection(Vector2 dashDir, Vector2 normal)
+    {
+        float dashDirNormalAngle = Mathf.Acos(Vector2.Dot(dashDir, normal));
+        float ninetyDegreesInRadians = Mathf.PI / 2;
+        float rotationRadians = Vector3.Cross(dashDir, normal).z < 0 ? ninetyDegreesInRadians - dashDirNormalAngle
+            : dashDirNormalAngle - ninetyDegreesInRadians;
+        return Vector2.Dot(dashDir, normal) < -.9f ? Vector2.zero : VectorUtil.RotateVector(dashDir, rotationRadians);
+    }
+
     void FixedUpdate ()
     {
         if (dashing)
         {
-            Player.PlayerTransform.position = Vector2.MoveTowards(Player.PlayerTransform.position, target,
+            Player.PlayerTransform.position = Vector2.MoveTowards(Player.PlayerTransform.position, waypoints[waypointIndex],
                 dashSpeed * Time.fixedDeltaTime);
-            if (Mathf.Abs(((Vector2)Player.PlayerTransform.position - target).sqrMagnitude) == 0)
+            if (Mathf.Abs(((Vector2)Player.PlayerTransform.position - waypoints[waypointIndex]).sqrMagnitude) == 0)
             {
-                dashing = false;
+                if (waypointIndex < waypoints.Count - 1)
+                {
+                    waypointIndex++;
+                } else
+                {
+                    dashing = false;
+                }
             }
         }
     }
@@ -89,6 +127,7 @@ public class Dash : Weapon {
         rb2d = Player.PlayerTransform.GetComponent<Rigidbody2D>();
         boxCollider2D = Player.PlayerTransform.GetComponent<BoxCollider2D>();
         dashing = false;
+        waypoints = new List<Vector2>();
     }
 
     void OnLevelWasLoaded ()
